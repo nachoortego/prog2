5114f245192967bfdd86b8678d9f276678b45051
Ignacio Ortego <ignacioortego110@gmail.com>
2023-11-11 17:33:19 -0300

    practica 2

--- practica1y2.c
+++ practica1y2.c
@@ -51,7 +51,7 @@ está contenida dentro de la otra.
 
 int ej18 (char word[], int largo, char c) {
     int found = 0;
-    for(int i = 0; i<largo; i++){
+    for(int i = 0; i<=largo; i++){
         if (word[i] == c) found = 1;
     }
 
@@ -62,7 +62,7 @@ int ej18 (char word[], int largo, char c) {
 
 int ej19 (char word[], int largo, char c) {
     int count = 0;
-    for(int i = 0; i<largo; i++){
+    for(int i = 0; i<=largo; i++){
         if (word[i] == c) count++;
     }
 
@@ -70,9 +70,56 @@ int ej19 (char word[], int largo, char c) {
 }
 
 void ej20 (char word[], int largo) {
-    for(int i = largo-1; i>=0; i--){
+    for(int i = largo; i>=0; i--){
         printf("%c", word[i]);
     }
+}
+
+int ej21 (char word[], int largo) {
+    int capicua = 1;
+    for(int i = 0, j = largo-1; i<largo; i++, j--){
+        printf("i = %c j = %c\n", word[i], word[j]);
+        if(word[i] != word[j]) capicua = 0;
+    }
+
+    return capicua;
+}
+
+int ej23 (char word1[], int largo1, char word2[], int largo2) {
+    int start = 0, value = 0;
+    for(int i = 0, j = 0; i<=largo1; i++){
+        if(word1[i] == word2[0]) start = 1;
+        if(start){
+            if (word1[i] == word2[j] && j!=largo2) value = 1, j++;
+            else if(j==largo2-1) value = 0, start = 0, j = 0;
+            //printf("i=%c j=%c\nstart=%d  value=%d\n\n", word1[i], word2[j], start, value);
+        }
+    }
+
+    return value;
+}
+
+/*Ejercicio 14. Escriba un programa que lea enteros hasta que se ingrese un número negativo
+y posteriormente imprima qué valor entre 0 y 99 se ingresó más veces*/
+
+int ej14 () {
+    int number = 100,total[100];
+    int max = -1;
+
+    for(int i=0;i<100;i++) {
+        total[i] = 0;
+    }
+
+    for(;number>=0;scanf("%d",&number)) {
+        total[number]++;
+    }
+
+    for(int i=0;i<100;i++) {
+        if(total[i]>max) max = total[i];
+    }
+
+
+    return max;
 }
 
 int main() {
@@ -83,7 +130,21 @@ int main() {
     //scanf("%i",&a);
     //printf("%s",ej13(a));
 
-    ej20("hola",5);
+    //ej20("hola",5);
+
+    //printf("%d\n",ej21("hola",4));
+    //printf("%d\n",ej21("salas",5));
+    //printf("%d\n",ej21("sallas",6));
+    //printf("%d\n",ej21("coco",4));
+
+    //printf("%d\n",ej23("hola",4,"ho",2));
+    //printf("%d\n",ej23("salas",5,"ala",3));
+    //printf("%d\n",ej23("sallas",6,"nashe",5));
+    //printf("%d\n",ej23("coco",4,"c",1));
+    //printf("%d\n",ej23("celeceste",9,"cel",3));
+
+    printf("%d\n",ej14());
+    
     return 0;
 }
 
